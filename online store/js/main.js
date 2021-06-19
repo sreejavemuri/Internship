518296617e0f3af9d3f127e6a8a000dabe8eedaf
sreejavemuri <sreejavemuri98@gmail.com>
2021-06-19 17:34:50 -0400

    added css, grid and changed the positioning of the containers and deleted few html pages as they were found unused

--- online store/js/main.js
+++ online store/js/main.js
@@ -1,7 +1,46 @@
-(function ($) {
-    "use strict";
-    
-    // Dropdown on mouse hover
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop  > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+// counter design
+ document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration /  range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
+ });
+   
+ // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 768) {
@@ -17,223 +56,5 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-    
-    
-    // Header slider
-    $('.header-slider').slick({
-        autoplay: true,
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-    
-    
-    // Product Slider 4 Column
-    $('.product-slider-4').slick({
-        autoplay: true,
-        infinite: true,
-        dots: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 4,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-        ]
-    });
-    
-    
-    // Product Slider 3 Column
-    $('.product-slider-3').slick({
-        autoplay: true,
-        infinite: true,
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-        ]
-    });
-    
-    
-    // Product Detail Slider
-    $('.product-slider-single').slick({
-        infinite: true,
-        autoplay: true,
-        dots: false,
-        fade: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.product-slider-single-nav'
-    });
-    $('.product-slider-single-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false,
-        centerMode: true,
-        focusOnSelect: true,
-        asNavFor: '.product-slider-single'
-    });
-    
-    
-    // Brand Slider
-    $('.brand-slider').slick({
-        speed: 5000,
-        autoplay: true,
-        autoplaySpeed: 0,
-        cssEase: 'linear',
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        infinite: true,
-        swipeToSlide: true,
-        centerMode: true,
-        focusOnSelect: false,
-        arrows: false,
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 4,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 300,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-    
-    
-    // Review slider
-    $('.review-slider').slick({
-        autoplay: true,
-        dots: false,
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-    
-    
-    // Widget slider
-    $('.sidebar-slider').slick({
-        autoplay: true,
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-    
-    
-    // Quantity
-    $('.qty button').on('click', function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
-        if ($button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find('input').val(newVal);
-    });
-    
-    
-    // Shipping address show hide
-    $('.checkout #shipto').change(function () {
-        if($(this).is(':checked')) {
-            $('.checkout .shipping-address').slideDown();
-        } else {
-            $('.checkout .shipping-address').slideUp();
-        }
-    });
-    
-    
-    // Payment methods show hide
-    $('.checkout .payment-method .custom-control-input').change(function () {
-        if ($(this).prop('checked')) {
-            var checkbox_id = $(this).attr('id');
-            $('.checkout .payment-method .payment-content').slideUp();
-            $('#' + checkbox_id + '-show').slideDown();
-        }
-    });
-})(jQuery);
+   
 
